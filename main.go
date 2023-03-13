b9fc8ce4720267d42cbd80f6b59548ec876c4d02
mihirpandya-greenops <mihir@greenops.io>
2023-03-13 01:01:58 -0700

    Add docs, goreleaser, and GH actions

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"greenops.io/terraform-provider-greenops/provider"
 )
 
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider.Provider,
